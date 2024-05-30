5711da0ad103209acdd57ec3f5fec3da725f3d74
Tatiana Medalith <107322613+TMedalith@users.noreply.github.com>
2024-05-30 11:52:45 -0500

    Update db.py

--- config/db.py
+++ config/db.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'eajwUQjoYCHCAxGfmMhfkzdcPshzzCdw'
-MYSQL_HOST = 'roundhouse.proxy.rlwy.net'
-MYSQL_PORT = '40438'
+MYSQL_PASSWORD = 'MmeGvXZLtObSxyOWGpsIpnMioRtpOJMl'
+MYSQL_HOST = 'viaduct.proxy.rlwy.net'
+MYSQL_PORT = '37129'
 MYSQL_DATABASE = 'railway'
 
 #URL_DATABASE = 'mysql+pymysql://root:12345@localhost:3306/veterinarys'
+#mysql://root:MmeGvXZLtObSxyOWGpsIpnMioRtpOJMl@viaduct.proxy.rlwy.net:37129/railway
 URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
 
 engine = create_engine(URL_DATABASE)
