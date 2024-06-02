2145ab2ed4acfdf3dfab8599aca42fc6daa15275
Tatiana Medalith <107322613+TMedalith@users.noreply.github.com>
2024-06-01 21:54:10 -0500

    Update db.py

--- config/db.py
+++ config/db.py
@@ -3,14 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'fvYQogwpIoplNmoKtNhgxvbhlHHQVvaS'
+MYSQL_PASSWORD = 'FfgUEZJRiDPRjPsYHlTaokDXYDAhjuHY'
 MYSQL_HOST = 'viaduct.proxy.rlwy.net'
-MYSQL_PORT = '58668'
+MYSQL_PORT = '58091'
 MYSQL_DATABASE = 'railway'
 #mysql://root:wUtLBypWHLWWnRUmdFtmdWszWZYYKoaL@viaduct.proxy.rlwy.net:36102/railway
 #URL_DATABASE = 'mysql+pymysql://root:12345@localhost:3306/veterinarys'
 #mysql://root:MmeGvXZLtObSxyOWGpsIpnMioRtpOJMl@viaduct.proxy.rlwy.net:37129/railway
 #mysql://root:fvYQogwpIoplNmoKtNhgxvbhlHHQVvaS@viaduct.proxy.rlwy.net:58668/railway
+#mysql://root:FfgUEZJRiDPRjPsYHlTaokDXYDAhjuHY@viaduct.proxy.rlwy.net:58091/railway
 URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
 
 engine = create_engine(URL_DATABASE)
